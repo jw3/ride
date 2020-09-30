d05c592c8fceee77d8087afcda076ba0cc9a5f41
John Wass <jwass3@gmail.com>
2020-09-29 23:02:57 -0400

    accurate tracking

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,8 @@ use geo::algorithm::line_interpolate_point::LineInterpolatePoint;
 use geo::prelude::*;
 use serde::Serialize;
 
+use std::iter::FromIterator;
+
 #[derive(Serialize)]
 struct Event {
     id: String,
@@ -18,8 +20,8 @@ struct Event {
     lat: String,
 }
 
-fn as_point(g: &(f64, f64, f64)) -> Point<f64> {
-    return Point::from([g.0, g.1]);
+fn as_point(c: (f64, f64, f64)) -> Point<f64> {
+    return Point::from([c.0, c.1]);
 }
 
 // todo;; get gdal::vector::ToGdal working
@@ -43,49 +45,40 @@ fn event(sp: &Point<f64>) {
 fn main() {
     let mut dataset = Dataset::open(Path::new(".local/test-ride.gpkg")).unwrap();
     let layer = dataset.layer(0).unwrap();
-    let srs = SpatialRef::from_epsg(32617).unwrap();
 
     for feature in layer.features() {
-        let _fname = feature.field("name").unwrap();
-        let geometry = feature.geometry().transform_to(&srs).unwrap();
-        for pt in geometry.get_point_vec().iter().map(as_point) {
-            println!("{}, {}", pt.0.x, pt.0.y);
-        }
-
-        let pv = feature.geometry().get_point_vec();
-        let p0 = pv.first().map(as_point).unwrap();
-        let p1 = pv.last().map(as_point).unwrap();
-        let d0 = p0.geodesic_distance(&p1);
+        let pv: Vec<Point<f64>> = feature.geometry().get_point_vec().into_iter().map(as_point).collect();
+        let p0 = pv.first().unwrap();
+        let p1 = pv.last().unwrap();
+        let l: LineString<f64> = LineString::from_iter(pv.iter().map(|p|p.0));
+        let d0 = l.geodesic_length();
         println!("{}m", d0.round());
 
         let kph = 20.0;
         let mps = kph / 3.6;
-        let sec = d0 / mps;
-        let stp = d0 / sec; // steps
+        let tts = d0 / mps;
+        let int = 2;
+        let fac = 100;
+        let stp = tts / int as f64;
         let pp = 100.0 / stp;
+        let step_length = time::Duration::from_millis(int * 1000 / fac);
 
-        let l: LineString<f64> = vec![[p0.0.x, p0.0.y],[p1.0.x, p1.0.y]].into();
-        let p2: Point<f64> = l.line_interpolate_point(&0.5).x_y().into();
-        let d1 = p0.geodesic_distance(&p2);
-        println!("{}m", d1.round());
-
-        assert_eq!(d0 as i64 / 2, d1 as i64);
-
-        let step_length = time::Duration::from_secs(2);
-
-        let sstp = stp as i64;
         println!("{}: {}m ({}%)", 0, 0.0, 0);
         event(&p0);
-        for s in 1..sstp {
-            thread::sleep(step_length);
-
+        thread::sleep(step_length);
+        let mut traveled = 0.0;
+        let mut previous = Point::new(p0.x_y().0, p0.x_y().1);
+        for s in 1..(stp as i64) {
             let p = s as f64 * pp / 100.0;
             let sp: Point<f64> = l.line_interpolate_point(&p).x_y().into();
-            let t = p0.geodesic_distance(&sp);
-            println!("{}: {:.1}m ({:.0}%) {} {}", s, t, p * 100.0, sp.x_y().0, sp.x_y().1);
+            traveled += previous.geodesic_distance(&sp);
+            previous = Point::new(sp.x_y().0, sp.x_y().1);
+            println!("{}: {:.1}m ({:.0}%)", s, traveled, p * 100.0);
             event(&sp);
+            thread::sleep(step_length);
         }
         println!("{}: {:.1}m ({}%)", 5, d0, 100);
         event(&p1);
+        thread::sleep(step_length);
     }
 }

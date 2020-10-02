78cbe37399bae165ee4272d1e01e738a78d78d59
John Wass <jwass3@gmail.com>
2020-10-02 01:40:57 -0400

    sketch in actors

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,68 @@ use serde::Serialize;
 
 use log::{debug, info, warn};
 
+use actix::prelude::*;
+use actix::clock::{delay_for, Duration};
+
+use reqwest::Client;
+
+struct Driver {
+    uri: Option<String>,
+    steptime: actix::clock::Duration,
+    current_step: u64,
+    total_steps: u64,
+    traveled: f64, // distance traveled in meters
+    previous_point: Option<Point<f64>>
+}
+
+impl Actor for Driver {
+    type Context = Context<Self>;
+
+    fn started(&mut self, _ctx: &mut Self::Context) {
+        println!("I am alive!");
+    }
+}
+
+impl Handler<WayPoint> for Driver {
+    type Result = ();
+
+    fn handle(&mut self, p: WayPoint, _ctx: &mut Context<Self>) {
+        let pct = (self.current_step as f64 / self.total_steps as f64) * 100.0;
+        let d = self.previous_point.map(|prev| prev.geodesic_distance(&p.pos)).unwrap_or(0.0);
+        self.traveled += d;
+
+        info!("{}: {:.1}m ({:.1}%)", self.current_step, self.traveled, pct);
+        self.current_step += 1;
+        self.previous_point = Some(p.pos);
+
+        let e = Event {
+            id: String::from("foo"),
+            lon: format!("{:.6}", p.pos.x_y().0),
+            lat: format!("{:.6}", p.pos.x_y().1)
+        };
+        let json = serde_json::to_string_pretty(&e).unwrap();
+
+        if let Some(uri) = &self.uri {
+            info!("{}", json);
+            let r = reqwest::Client::new().post(uri).json(&e).send();
+            // if let Err(e) = r {
+            //     warn!("{}", e);
+            // }
+        }
+        else {
+            println!("{}", json);
+        }
+
+        delay_for(self.steptime);
+    }
+}
+
+#[derive(Message)]
+#[rtype(result = "()")]
+struct WayPoint {
+    pos: Point<f64>
+}
+
 #[derive(Serialize)]
 struct Event {
     id: String,
@@ -21,26 +83,6 @@ struct Event {
 
 fn as_point(c: (f64, f64, f64)) -> Point<f64> {
     return Point::from([c.0, c.1]);
-}
-
-fn event(sp: &Point<f64>, opt_uri: &Option<String>) {
-    let e = Event {
-        id: String::from("foo"),
-        lon: format!("{:.6}", sp.x_y().0),
-        lat: format!("{:.6}", sp.x_y().1)
-    };
-    let json = serde_json::to_string_pretty(&e).unwrap();
-
-    if let Some(uri) = opt_uri {
-        info!("{}", json);
-        let r = reqwest::blocking::Client::new().post(uri).json(&e).send();
-        if let Err(e) = r {
-            warn!("{}", e);
-        }
-    }
-    else {
-        println!("{}", json);
-    }
 }
 
 /// Simulated sensor riding along geo features.
@@ -71,6 +113,8 @@ fn main() {
     let opts: Opts = Opts::parse();
     env_logger::init();
 
+    let system = actix::System::new("ride");
+
     let mut dataset = Dataset::open(Path::new(&opts.gpkg)).unwrap();
     let layer = dataset.layer(0).unwrap();
 
@@ -90,27 +134,28 @@ fn main() {
         let ppu = 100.0 / stp;        // percent per update
 
         // use the factor value to increase the playback speed
-        let step_length = time::Duration::from_millis(int * 1000 / opts.factor);
+        let step_length = actix::clock::Duration::from_millis(int * 1000 / opts.factor);
 
-        info!("{}: {}m ({}%)", 0, 0.0, 0);
+        let d = Driver {
+            uri: opts.uri.clone(),
+            steptime: step_length,
+            current_step: 0,
+            total_steps: stp as u64,
+            traveled: 0.0,
+            previous_point: None
+        };
+        let a = d.start();
+
         let p0 = pv.first().unwrap();
-        event(&p0, &opts.uri);
-        thread::sleep(step_length);
-
-        let mut traveled = 0.0;
-        let mut previous = Point::new(p0.x_y().0, p0.x_y().1);
+        a.do_send(WayPoint{ pos: *p0 });
         for s in 1..(stp as i64) {
             let p = s as f64 * ppu;
             let sp: Point<f64> = l.line_interpolate_point(&(p / 100.0)).x_y().into();
-            traveled += previous.geodesic_distance(&sp);
-            previous = sp;
-            info!("{}: {:.1}m ({:.0}%)", s, traveled, p);
-            event(&sp, &opts.uri);
-            thread::sleep(step_length);
+            a.do_send(WayPoint{ pos: sp });
         }
-        info!("{}: {:.1}m ({}%)", 5, d0, 100);
         let p1 = pv.last().unwrap();
-        event(&p1, &opts.uri);
-        thread::sleep(step_length);
+        a.do_send(WayPoint{ pos: *p1 });
     }
+
+    system.run();
 }
